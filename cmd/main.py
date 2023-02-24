740ee1047c1e43022ce0493cea8d438d8966f5e7
NurzhauganovA <90419990+NurzhauganovA@users.noreply.github.com>
2023-02-24 15:30:50 +0600

    Second neuron. Determine Image

--- cmd/main.py
+++ cmd/main.py
@@ -1,19 +1,96 @@
+import keras
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = [
+    'T-shirt/top',
+    'Trouser',
+    'Pullover',
+    'Dress',
+    'Coat',
+    'Sandal',
+    'Shirt',
+    'Sneaker',
+    'Bag',
+    'Ankle boot'
+]
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
+
+model = keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('Test accuracy: ', test_acc)
+
+predictions = model.predict(test_images)
+print(predictions[0])
+
+print(np.argmax(predictions[0]))
 
 
-celsius_q = np.array([11, 13, 14, 16, 16, 14, 17], dtype=float)
-fahrenheit_a = np.array([51.8, 55.4, 57.2, 60.8, 60.8, 57.4, 62.6], dtype=float)
+def plot_image(i, predictions_array, true_label, img):
+    true_label, img = true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
 
-# for i, c in enumerate(celsius_q):
-#     print(f'{c} degrees Celsius = {fahrenheit_a[i]} degrees Fahrenheit')
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100 * np.max(predictions_array),
+                                         class_names[true_label]),
+               color=color)
 
 
-l0 = tf.keras.layers.Dense(units=1, input_shape=[1])
-model = tf.keras.Sequential([l0])
-model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.1))
+def plot_value_array(i, predictions_array, true_label):
+    true_label = true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
 
-history = model.fit(celsius_q, fahrenheit_a, epochs=5000, verbose=False)
-print('Finished training the model')
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
 
-print(model.predict([20]))
+
+i = 10  # Index of image ()
+plt.figure(figsize=(6, 3))
+plt.subplot(1, 2, 1)
+plot_image(i, predictions[i], test_labels, test_images)
+plt.subplot(1, 2, 2)
+plot_value_array(i, predictions[i], test_labels)
+plt.show()
